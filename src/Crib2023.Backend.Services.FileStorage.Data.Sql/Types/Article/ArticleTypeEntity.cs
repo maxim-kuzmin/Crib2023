d027366320990480f48f046bb5d8ea1fbbe5a23a
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-02-05 23:48:39 +0300

    -

---
+++ src/Crib2023.Backend.Services.FileStorage.Data.Sql/Types/Article/ArticleTypeEntity.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2023 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
+
+namespace Crib2023.Backend.Services.FileStorage.Data.Sql.Types.Article;
+
+/// <summary>
+/// Сущность типа "Статья".
+/// </summary>
+public class ArticleTypeEntity
+{
+    #region Properties
+
+    /// <summary>
+    /// Хэш.
+    /// </summary>
+    public string? Hash { get; set; }
+
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Путь.
+    /// </summary>
+    public string? Path { get; set; }
+
+    /// <summary>
+    /// Заголовок.
+    /// </summary>
+    public string? Title { get; set; }
+
+    /// <summary>
+    /// Идентификатор экземпляра сущности "Тема".
+    /// </summary>
+    public long TopicId { get; set; }
+
+    #endregion Properties
+}
---
+++ src/Crib2023.Backend.Services.FileStorage.Data.Sql/Types/Topic/TopicTypeEntity.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) 2023 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
+
+namespace Crib2023.Backend.Services.FileStorage.Data.Sql.Types.Topic;
+
+/// <summary>
+/// Сущность типа "Тема".
+/// </summary>
+public class TopicTypeEntity
+{
+    #region Properties
+
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Идентификатор родителя.
+    /// </summary>
+    public long? ParentId { get; set; }
+
+    /// <summary>
+    /// Число детей в дереве.
+    /// </summary>
+    public long TreeChildCount { get; set; }
+
+    /// <summary>
+    /// Число потомков в дереве.
+    /// </summary>
+    public long TreeDescendantCount { get; set; }
+
+    /// <summary>
+    /// Уровень в дереве.
+    /// </summary>
+    public long TreeLevel { get; set; }
+
+    /// <summary>
+    /// Путь в дереве.
+    /// </summary>
+    public string? TreePath { get; set; }
+
+    /// <summary>
+    /// Позиция в дереве.
+    /// </summary>
+    public int TreePosition { get; set; }
+
+    /// <summary>
+    /// Сортировка в дереве.
+    /// </summary>
+    public string? TreeSort { get; set; }
+
+    #endregion Properties
+}
