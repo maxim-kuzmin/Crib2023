9901d5ebd6f960bf6f1cd5e26c0f98d3290d4a42
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-02-06 18:54:40 +0300

    -

--- src/Backend/Services/FileStorage/Data.Sql.Clients.PostgreSql/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) 2023 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
-
-global using Crib2023.Backend.Services.FileStorage.Data.Sql.Types.Article;
-global using Crib2023.Backend.Services.FileStorage.Data.Sql.Types.Topic;
-global using Makc2023.Backend.Common.Core.App;
-global using Makc2023.Backend.Common.Data.Clients.PostgreSql;
-global using Microsoft.Extensions.DependencyInjection;
