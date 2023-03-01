838ead76d024d2fd0e0b1b241e9e5bf4149eef84
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-03-01 19:16:23 +0300

    -

--- src/Frontend/React/TypeScript/App/src/App.tsx
+++ src/Frontend/React/TypeScript/App/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 12
         </a>
       </header>
     </div>
