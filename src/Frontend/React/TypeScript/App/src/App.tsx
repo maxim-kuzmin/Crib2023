17744421ef764987b96fcf31939af17706cfefe6
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-03-09 02:40:02 +0300

    -

--- src/Frontend/React/TypeScript/App/src/App.tsx
+++ src/Frontend/React/TypeScript/App/src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
+import { Layout, theme } from 'antd';
+import styles from './App.module.css';
 import logo from './logo.svg';
-import './App.css';
+import ArticleTable from './components/ArticleTable';
+import TopicPath from './components/TopicPath';
+import TopicTree from './components/TopicTree';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 function App () {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 12
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+        style={{ padding: '16px' }}
+      >
+        <div className={styles.logo}>
+          <div className={styles['logo-image']} style={{ backgroundImage: `url(${logo})` }}/>
+        </div>
+        <TopicTree/>
+      </Sider>
+      <Layout>
+        <Header style={{ padding: '0 16px', background: colorBgContainer }}>
+          <TopicPath/>
+        </Header>
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+            <ArticleTable/>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
---
+++ src/Frontend/React/TypeScript/App/src/components/TopicPath/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './TopicPath.module.css';
+
+export default function TopicTree () {
+  return (
+    <div className={styles.root}>
+      Topic Path
+    </div>
+  );
+}
