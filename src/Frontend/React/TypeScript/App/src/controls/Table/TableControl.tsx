0f31ba17750dcf2d1814d1d80a407c874dd47b8b
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-04-05 02:24:54 +0300

    -

---
+++ src/Frontend/React/TypeScript/App/src/controls/Table/TableControl.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Table } from 'antd';
+
+export const TableControl: React.FC = () => {
+  return <Table/>
+}
--- src/Frontend/React/TypeScript/App/src/controls/index.ts
+++ src/Frontend/React/TypeScript/App/src/controls/index.ts
@@ -2,4 +2,5 @@ export * from './Breadcrumb';
 export * from './Layout';
 export * from './Notification';
 export * from './Spinner';
+export * from './Table';
 export * from './Tree';
