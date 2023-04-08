bedad5c80326e5ecd314bf2f4b22b4b777d806a6
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-04-09 01:22:53 +0300

    -

--- src/Frontend/React/TypeScript/App/src/App.tsx
+++ src/Frontend/React/TypeScript/App/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import {
-  AppLayoutAsideView,
-  AppLayoutContentView,
-  AppLayoutFooterView,
-  AppLayoutHeaderView,
-  AppNotificationView,
-  LayoutControl
-} from './all';
+import { AppNotificationView } from './views/App/Notification';
+import { LayoutControl } from './controls/Layout/LayoutControl';
+import { AppLayoutAsideView } from './views/App/Layout/Aside';
+import { AppLayoutContentView } from './views/App/Layout/Content';
+import { AppLayoutFooterView } from './views/App/Layout/Footer';
+import { AppLayoutHeaderView } from './views/App/Layout/Header';
 
 export const App: React.FC = () => {
   return (
