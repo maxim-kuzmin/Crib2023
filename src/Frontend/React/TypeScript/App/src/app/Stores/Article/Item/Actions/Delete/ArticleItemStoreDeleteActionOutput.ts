613e3c05e03d0d4d5791e13147146da87905a602
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-04-19 04:57:53 +0300

    -

--- src/Frontend/React/TypeScript/App/src/app/Stores/Article/Item/Actions/Delete/ArticleItemStoreDeleteActionOutput.ts
+++ src/Frontend/React/TypeScript/App/src/app/Stores/Article/Item/Actions/Delete/ArticleItemStoreDeleteActionOutput.ts
@@ -1,3 +1,6 @@
+import { type ArticleItemStoreDeleteCompletedActionPayload } from '../DeleteCompleted';
+
 export interface ArticleItemStoreDeleteActionOutput {
   readonly loading: boolean;
+  readonly payload: ArticleItemStoreDeleteCompletedActionPayload;
 }
--- src/Frontend/React/TypeScript/App/src/views/Article/Item/ArticleItemViewHooks.ts
+++ src/Frontend/React/TypeScript/App/src/views/Article/Item/ArticleItemViewHooks.ts
@@ -1,22 +1,45 @@
 import {
   type ArticleItemStoreClearActionDispatch,
   type ArticleItemStoreClearActionOptions,
+  type ArticleItemStoreDeleteActionDispatch,
+  type ArticleItemStoreDeleteActionInput,
+  type ArticleItemStoreDeleteActionOptions,
+  type ArticleItemStoreDeleteActionOutput,
+  type ArticleItemStoreDeleteCompletedActionDispatch,
+  type ArticleItemStoreDeleteCompletedActionOptions,
   type ArticleItemStoreLoadActionDispatch,
   type ArticleItemStoreLoadActionInput,
   type ArticleItemStoreLoadActionOptions,
   type ArticleItemStoreLoadActionOutput,
+  type ArticleItemStoreSaveActionDispatch,
+  type ArticleItemStoreSaveActionInput,
+  type ArticleItemStoreSaveActionOptions,
+  type ArticleItemStoreSaveActionOutput,
   type ArticleItemStoreSetActionDispatch,
   type ArticleItemStoreSetActionOptions,
-  type ArticleItemStoreState,
+  type ArticleItemStoreState
 } from '../../../app/Stores';
 
 type ClearActionDispatch = ArticleItemStoreClearActionDispatch;
 type ClearActionOptions = ArticleItemStoreClearActionOptions;
 
+type DeleteActionDispatch = ArticleItemStoreDeleteActionDispatch;
+type DeleteActionInput = ArticleItemStoreDeleteActionInput;
+type DeleteActionOptions = ArticleItemStoreDeleteActionOptions;
+type DeleteActionOutput = ArticleItemStoreDeleteActionOutput;
+
+type DeleteCompletedActionDispatch = ArticleItemStoreDeleteCompletedActionDispatch;
+type DeleteCompletedActionOptions = ArticleItemStoreDeleteCompletedActionOptions;
+
 type LoadActionDispatch = ArticleItemStoreLoadActionDispatch;
 type LoadActionInput = ArticleItemStoreLoadActionInput;
 type LoadActionOptions = ArticleItemStoreLoadActionOptions;
 type LoadActionOutput = ArticleItemStoreLoadActionOutput;
+
+type SaveActionDispatch = ArticleItemStoreSaveActionDispatch;
+type SaveActionInput = ArticleItemStoreSaveActionInput;
+type SaveActionOptions = ArticleItemStoreSaveActionOptions;
+type SaveActionOutput = ArticleItemStoreSaveActionOutput;
 
 type SetActionDispatch = ArticleItemStoreSetActionDispatch;
 type SetActionOptions = ArticleItemStoreSetActionOptions;
@@ -25,6 +48,11 @@ type StoreState = ArticleItemStoreState;
 
 export interface ArticleItemViewHooks {
   readonly useClearActionDispatch: (options: ClearActionOptions) => ClearActionDispatch;
+  readonly useDeleteActionDispatch: (options: DeleteActionOptions) => DeleteActionDispatch;
+  readonly useDeleteActionOutput: (input: DeleteActionInput) => DeleteActionOutput;
+  readonly useDeleteCompletedActionDispatch: (options: DeleteCompletedActionOptions) => DeleteCompletedActionDispatch;
+  readonly useSaveActionDispatch: (options: SaveActionOptions) => SaveActionDispatch;
+  readonly useSaveActionOutput: (input: SaveActionInput) => SaveActionOutput;
   readonly useLoadActionDispatch: (options: LoadActionOptions) => LoadActionDispatch;
   readonly useLoadActionOutput: (input: LoadActionInput) => LoadActionOutput;
   readonly useSetActionDispatch: (options: SetActionOptions) => SetActionDispatch;
