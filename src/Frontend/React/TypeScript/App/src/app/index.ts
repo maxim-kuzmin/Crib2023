1f35f1b117565c07f5e98ec7ddfdb9ab5561136d
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-05-02 16:02:21 +0300

    -

--- src/Frontend/React/TypeScript/App/src/app/index.ts
+++
@@ -1,6 +0,0 @@
-export { type AppComponent } from './AppComponent';
-export { type AppControl } from './AppControl';
-export { type AppFactory } from './AppFactory';
-export { type AppHooks } from './AppHooks';
-export { type AppInstance } from './AppInstance';
-export { type AppModule } from './AppModule';
